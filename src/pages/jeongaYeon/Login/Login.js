0cd6355b77af8ea7474f692641cfcf24e70fd346
joyfuldev <wings2132@naver.com>
2023-04-20 16:36:13 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jeongaYeon/Login/Login.js
+++ src/pages/jeongaYeon/Login/Login.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
 import { useNavigate } from 'react-router-dom';
 
 const LoginJeonga = () => {
+  const [id, setId] = useState();
+  const [password, setPassword] = useState();
   const navigate = useNavigate();
+
   const handleLogin = () => {
     navigate('/main-jeonga');
   };
+
+  const saveUserId = event => {
+    let userId = event.target.value;
+    setId(userId);
+  };
+
+  const saveUserPassword = event => {
+    let userPassword = event.target.value;
+    setPassword(userPassword);
+  };
+
   return (
     <main className="container">
       <div className="wrapper">
@@ -15,6 +29,7 @@ const LoginJeonga = () => {
             <h1 className="loginTitle">Westagram</h1>
             <div className="loginIdWrapper">
               <input
+                onChange={saveUserId}
                 placeholder="전화번호, 사용자 이름 또는 이메일"
                 type="text"
                 className="loginId"
@@ -22,6 +37,7 @@ const LoginJeonga = () => {
             </div>
             <div className="loginPasswordWrapper">
               <input
+                onChange={saveUserPassword}
                 className="loginPassword"
                 placeholder="비밀번호"
                 type="password"
