4094f61ad84041971c3adb0d598079b14d8d20e4
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-04-21 22:19:31 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/sujeongKim/Login/Login.js
+++ src/pages/sujeongKim/Login/Login.js
@@ -3,20 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const LoginSujeong = () => {
-  const [UserId, setUserId] = useState('');
-  const [UserPw, setUserPw] = useState('');
+  const [userId, setUserId] = useState('');
+  const [userPw, setUserPw] = useState('');
   const navigate = useNavigate();
 
-  const saveUserId = event => {
-    setUserId(event.target.value);
-  };
-
-  const saveUserPw = event => {
-    setUserPw(event.target.value);
-  };
-
   const goToMain = () => {
-    navigate('./main-sujeong');
+    navigate('/Main');
   };
 
   return (
@@ -29,21 +21,40 @@ const LoginSujeong = () => {
             className="loginId loginInput"
             name="userId"
             placeholder="전화번호, 사용자 이름 또는 이메일"
-            onChange={saveUserId}
+            onChange={e => {
+              setUserId(e.target.value);
+            }}
           />
           <input
             type="password"
             className="loginPw loginInput"
             name="userPw"
             placeholder="비밀번호"
-            onChange={saveUserPw}
+            onChange={e => {
+              setUserPw(e.target.value);
+            }}
           />
-          <input
-            type="button"
-            className="loginBtn"
-            value="로그인"
-            onClick={goToMain}
-          />
+          {userId.includes('@') && userPw.length >= 5 ? (
+            <button
+              disabled={false}
+              className="loginBtn loginBtnOn"
+              onClick={() => {
+                goToMain();
+              }}
+            >
+              로그인
+            </button>
+          ) : (
+            <button
+              disabled={true}
+              className="loginBtn"
+              onClick={() => {
+                goToMain();
+              }}
+            >
+              로그인
+            </button>
+          )}
         </form>
         <a href="/" className="findPw">
           비밀번호를 잊으셨나요?
