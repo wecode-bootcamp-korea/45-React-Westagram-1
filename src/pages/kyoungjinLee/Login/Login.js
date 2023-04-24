73b43e9dfb5e5b430fdc6431c12dcff6057aa2e9
Jinny-Jin <katie9796@naver.com>
2023-04-24 21:54:59 +0900

    수정사항2 : 불필요한 주석 제거

--- src/pages/kyoungjinLee/Login/Login.js
+++ src/pages/kyoungjinLee/Login/Login.js
@@ -3,26 +3,6 @@ import '../../../styles/common.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
-/*function Inputs() {
-  return (
-    <div class="inputs">
-      <input
-        id="Id"
-        type="text"
-        placeholder="  전화번호, 사용자 이름 또는 이메일"
-      />
-      <input id="Password" type="password" placeholder="  비밀번호" />
-      <button id="login-button" class="noLogin" onClick={handleLogin}>
-        로그인
-      </button>
-    </div>
-  );
-}
-
-function Westagram() {
-  return <div class="westagram">Westagram</div>;
-}*/
-
 function Footer() {
   return (
     <footer>
