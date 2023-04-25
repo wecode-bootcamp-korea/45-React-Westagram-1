e2626ad46716da248b5c6f7d104a3040c42f81cf
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-04-25 21:00:50 +0900

    1-3 파일 분리

--- src/pages/sujeongKim/Footer.js
+++ src/pages/sujeongKim/Footer.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './footer.scss';
+
 const Footer = () => {
+  const [footerItem, setFooterItem] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/footer.json')
+      .then(res => res.json())
+      .then(data => {
+        setFooterItem(data);
+      });
+  }, []);
+
   return (
     <footer className="mainFooter">
       <ul className="footerTop">
-        {FOOTER_LIST.map(item => {
+        {footerItem.map(item => {
           return (
             <li key={item.id}>
               <a>{item.listItem}</a>
@@ -17,17 +28,3 @@ const Footer = () => {
   );
 };
 export default Footer;
-
-const FOOTER_LIST = [
-  { id: 0, listItem: '소개' },
-  { id: 1, listItem: '도움말' },
-  { id: 2, listItem: '홍보 센터' },
-  { id: 3, listItem: 'API' },
-  { id: 4, listItem: '채용 정보' },
-  { id: 5, listItem: '개인정보처리방침' },
-  { id: 6, listItem: '약관' },
-  { id: 7, listItem: '위치' },
-  { id: 8, listItem: '인기 계정' },
-  { id: 9, listItem: '해시태그' },
-  { id: 10, listItem: '언어' },
-];
